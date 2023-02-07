54898c3715d0f3e4660fa96541c5effc73657e99
azimjonking <azimjonjalilov1301@mail.ru>
2023-02-07 19:56:36 +0200

    educational website is started

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/*============== SHOW MENU ==============*/
+
+/*===== MENU SHOW =====*/
+/* validate if constats exists */
+
+/*===== MENU HIDDEN =====*/
+/* validate if constats exists */
+
+/*============== CHANGE BACKGROUND HEADER ==============*/
